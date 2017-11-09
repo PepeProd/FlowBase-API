9ad05b04c3ca082b88a222e225fe8a741774f6dc
Bryan Smith <bryanj.smith91@gmail.com>
2017-11-08 23:22:17 -0800

    Made server listen to everything on 5000

--- Program.cs
+++ Program.cs
@@ -20,6 +20,7 @@ namespace FlowBaseAPI
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://*:5000;")
                 .Build();
     }
 }
